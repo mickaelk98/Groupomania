b863dc1090de0ade7b96a71402bbea8c159c9636
mickael keita <keitamickael@gmail.com>
2022-07-01 14:49:55 +0200

    Connexion a la base de donnée mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan')
+require('./config/db.config')
 
 
 const app = express();
---
+++ backend/config/db.config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+//* connection a la base de donnée mongoDB via mongoose
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.3ikrah1.mongodb.net/?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
