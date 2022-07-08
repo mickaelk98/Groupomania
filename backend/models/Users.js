60be1aebfe4b2df2919b026bb69ff5b6b0e63dfe
mickael keita <keitamickael@gmail.com>
2022-07-08 11:43:31 +0200

    amelioration des message d'erreur

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -5,27 +5,28 @@ const { isEmail } = require('validator')
 const userSchema = mongoose.Schema({
     firstName: {
         type: String,
-        required: true,
-        maxLength: 50,
+        required: [true, "Vous devez rentrer un prenom"],
+        maxlength: [50, "Ce champ doit faire moin de 50 caractere"]
     },
     lastName: {
         type: String,
-        required: true,
-        maxLength: 50,
+        required: [true, "Vous devez rentrez un nom"],
+        maxlength: [50, "Ce champ doit faire moin de 50 caractere"]
     },
     email: {
         type: String,
-        required: true,
-        unique: true,
-        validate: [isEmail],
+        required: [true, "Vous devez rentrer une adresse email"],
+        unique: [true],
+        validate: [isEmail, "L'adresse que vous avez rentrez ne correspond pas a un format d'email"]
     },
     password: {
         type: String,
-        required: true,
+        required: [true, "Vous devez rentrez un mot de passe"],
+        minlength: [8, "Votre mot de passe doit faire au moin 8 caractères"]
     },
     description: {
         type: String,
-        maxLength: 1000,
+        maxlength: [1000, "Votre description ne doit pas depasser 1000 caractères"]
     },
     image: {
         type: String,
@@ -40,6 +41,6 @@ const userSchema = mongoose.Schema({
         timestamps: true,
     })
 
-userSchema.plugin(uniqueValidator);
+userSchema.plugin(uniqueValidator, { message: "Cette email est déja" });
 
 module.exports = mongoose.model('User', userSchema);
