8dcb3877f91e114285f54a67577d3b64e52f1258
mickael keita <keitamickael@gmail.com>
2022-07-19 13:23:46 +0200

    affcihage des posts coé front

--- frontend/src/components/NewPost.vue
+++ frontend/src/components/NewPost.vue
@@ -1,12 +1,41 @@
 <script setup>
+import { reactive } from 'vue';
+
+
+// recuperation du userId et du token 
+const auth = JSON.parse(localStorage.getItem('auth'));
+const localUserId = auth.userId;
+const userToken = auth.token;
+console.log(auth, localUserId, userToken);
+
+// gere l'éta des donnée
+const state = reactive({
+    user: {}
+});
+
+// recuperation des donnée de l'utilisateur
+const getUserProfil = async () => {
+    const response = await fetch('http://localhost:5000/api/auth/'+ localUserId, {
+        headers: {
+            'content-Type': 'application/json',
+            'Authorization': 'Bearer ' + userToken,
+        }
+    });
+
+    const user = await response.json();
+    state.user = user;
+    console.log(state.user);
+}
+
+getUserProfil();
 </script>
 
 <template>
     <div class="new-post">
             <form>
                 <div class="img-name">
-                    <img src="../assets/images/default.jpg" alt="photo de profil">
-                    <p>john doe</p>
+                    <img :src="state.user.image" alt="photo de profil">
+                    <p>{{ state.user.firstName }} {{ state.user.lastName }}</p>
                 </div>
                 <textarea placeholder="Faite un nouveaux post"></textarea>
                 <input type="file">
--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,11 +1,46 @@
 <script setup>
+import { reactive } from 'vue';
+
+// recuperation du userId et du token 
+const auth = JSON.parse(localStorage.getItem('auth'));
+const localUserId = auth.userId;
+const userToken = auth.token;
+console.log(auth, localUserId, userToken);
+
+// gere l'éta des donnée
+const state = reactive({
+    posts: []
+});
+
+// recuperation des donnée de l'utilisateur
+const getAllPost = async () => {
+    const response = await fetch('http://localhost:5000/api/post/', {
+        headers: {
+            'content-Type': 'application/json',
+        }
+    });
+
+    const posts = await response.json();
+  
+    if (posts) {
+        if (Array.isArray(posts)) {
+            state.posts = posts;
+        } else {
+            state.posts = [posts];
+        }
+    }
+    console.log(state.posts);
+}
+
+getAllPost();
 </script>
 
 <template>
-    <div class="post">
+    <div class="post" v-for="post in state.posts" :key="post._id">
         <div class="img-name">
             <img src="../assets/images/default.jpg" alt="photo de profil">
             <p>john doe</p>
+            <p class="post-date">{{ Date(post.timestamp).split('GMT')[0]  }}</p>
         </div>
 
         <!-- boutton de modification et suppression de post -->
@@ -15,20 +50,17 @@
         </div>
 
         <!-- image du post -->
-        <img src="../assets/images/default.jpg" alt="image du post" class="post-image">
+        <img :src="post.image" alt="image du post" class="post-image" v-if="post.image">
 
         <!-- message du post -->
         <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Praesentium dolore quibusdam deleniti voluptas illum eaque?
-             Voluptas saepe facilis nostrum repudiandae quasi accusamus at 
-            vitae neque, assumenda sit illum perferendis beatae.
+            {{ post.text }}
         </p>
 
         <!-- like et commentaire -->
         <div class="like-comment">
-            <span><i class="far fa-heart"></i> 50 likes </span>
-            <span><i class="far fa-comment"></i> commentaire</span>
+            <span><i class="far fa-heart"></i> {{ post.likes }} likes </span>
+            <span><i class="far fa-comment"></i> {{ post.comments.length }} commentaires</span>
         </div>
 
         <!-- mettre un commentaire -->
@@ -38,23 +70,18 @@
         </form>
 
         <!-- commentaire du post -->
-        <div class="all-comment" >
-            <p class="comment">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                nblanditiis veritatis asperiores sapiente amet culpa nobis?
-            </p>
-            <p class="comment">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                nblanditiis veritatis asperiores sapiente amet culpa nobis?
-            </p>
-            <p class="comment">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                nblanditiis veritatis asperiores sapiente amet culpa nobis?
-            </p>
-            <p class="comment">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                nblanditiis veritatis asperiores sapiente amet culpa nobis?
-            </p>
+        <!-- <div class="all-comment" v-for="comment in state.posts.comments"> -->
+        <div class="all-comment" v-for="comment in post.comments" v-if="post.comments.length > 0">
+            <div class="comment-block">
+                <div class="commenter-info">
+                    <img :src="comment.commenterImage" alt="photo de profil" class="logo-img">
+                    <p>{{ comment.commenterLastName }} {{ comment.commenterFirstName }}</p>
+                </div>
+                <span>{{ Date(comment.timestamp).split('GMT')[0] }}</span>
+                <p class="comment">
+                    {{ comment.text }}
+                </p>
+            </div>
         </div>
     </div>
 </template>
